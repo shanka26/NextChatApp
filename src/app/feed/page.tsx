167447da34c5c5d8f4461bd061e3519077a8ca08
shanka26 <47332795+shanka26@users.noreply.github.com>
2024-07-13 08:36:38 -0500

    feed

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -36,8 +36,8 @@ export default function Page() {
     
     <div  className="grid grid-cols-12 max-h-screen justify-center bg-base-300">
 
-      <div className="md:col-span-2  col-span-auto bg-blue-950"></div>
-      <div className=" col-span-12 md:col-span-10 flex flex-col w-full max-h-[92vh] md:max-h-[90vh] justify-between overflow-hidden bg-neutral ">
+      <div className="col-span-auto md:col-span-2  "></div>
+      <div className=" col-span-12 md:col-span-8 flex flex-col w-full max-h-[92vh] md:max-h-[90vh] justify-between overflow-hidden  ">
         <div ref={chatRef} className="flex-grow h-screen  overflow-y-auto p-2 items-end ">
           {messageList.map((post: any, i: number) => (
             <div key={i} className="p-2">
@@ -46,7 +46,7 @@ export default function Page() {
           ))}
         </div>
 
-        <div className="fixed bottom-0 flex border w-[100vw] md:w-[60vw] md:max-h-[10vh] p-3  justify-between gap-2 bg-primary rounded-sma">
+        <div className="fixed bottom-0 flex border w-[100vw] md:w-[66.7%] col-span-12 md:col-span-8 md:max-h-[10vh] p-3  justify-between gap-2 bg-primary rounded-sma">
           <input
             disabled={!loggedIn}
             value={loggedIn ? message : "Please Sign In"}
@@ -66,7 +66,7 @@ export default function Page() {
           </button>
         </div>
       </div>
-      <div className="  col-span-auto  md:col-span-2"></div>
+      <div className="  col-span-auto md:col-span-2"></div>
     </div>
   );
 }
